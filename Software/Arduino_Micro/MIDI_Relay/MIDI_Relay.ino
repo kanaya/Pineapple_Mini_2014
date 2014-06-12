7a7a8171aafa4bdbbbd8264c3962867690cc303b
Ichi Kanaya <kanaya@pineapple.cc>
2014-06-12 09:07:13 +0900

    Added copyright notice to the source code.

--- Software/Arduino_Micro/MIDI_Relay/MIDI_Relay.ino
+++ Software/Arduino_Micro/MIDI_Relay/MIDI_Relay.ino
@@ -1,7 +1,18 @@
-// MIDI_Relay
-// Pineapple Mini 2014 / Arduino Micro
-// Written by Ichi Kanaya
-// Copyright (C) 2014, All rights reserved.
+// MIDI_Relay Copyright (C) 2014 Ichi Kanaya
+// For Pineapple Mini 2014 / Arduino Micro
+
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // Door opener is activated: A0 = 5V
 // Door opener is not activated: A0 = 0V
